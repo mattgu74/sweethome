36196ba7155bd9eb94c6821c4290bab1ceb9d23b
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2014-06-01 23:31:36 +0200

    Add some inputs & button

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 App.controller('home', function (page) {
-  // put stuff here
-});
-
-App.controller('page2', function (page) {
-  // put stuff here
+	var $page = $(page);
+	var $mens_input = $page.find('#mens-input');
+	var $ladies_input = $page.find('#ladies-input')
+	var $go_button = $page.find('#go-button');
+	var $result = $page.find('#result');
+	
+	var is_a_boy = true;
+	
+	$mens_input.on('click', function() {
+		$mens_input.attr("src", "mens_black.png");
+		$ladies_input.attr("src", "ladies_white.png");
+		is_a_boy = true;
+	});
+	
+	$ladies_input.on('click', function() {
+		$mens_input.attr("src", "mens_white.png");
+		$ladies_input.attr("src", "ladies_black.png");
+		is_a_boy = false;
+	});
+	
+	$go_button.on('click', function() {
+		if (is_a_boy) {
+			$result.html("You may not be safe !!");
+		}
+		else {
+			$result.html("Go on !!");
+		}
+	});
 });
 
 try {
-  App.restore();
-} catch (err) {
-  App.load('home');
+	App.restore();
+}
+catch (err) {
+	App.load('home');
 }
