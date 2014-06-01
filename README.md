5856d9bb5cbf2b72bbcff91735a818642524f06c
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-02 01:03:40 +0200

    add phpclient

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ sweethome
 =========
 
 Estimez votre alcoolémie en fonction de votre consommation payutc en temps réel !
+
+Install
+*******
+
+curl -sS https://getcomposer.org/installer | php
+
+./composer.phar install
--- script.js
+++ script.js
@@ -2,11 +2,33 @@ App.controller('home', function (page) {
   // put stuff here
 });
 
-App.controller('page2', function (page) {
+App.controller('apropos', function (page) {
   // put stuff here
 });
 
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+var params = getSearchParameters();
+
+function login() {
+  console.log(params);
+}
+
 try {
+  login();
   App.restore();
 } catch (err) {
   App.load('home');
